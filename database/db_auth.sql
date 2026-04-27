13c3188ed8864e527cd5bfdebddbaf2fc025f34c
Najwa-AP <najwaazahra16102006@gmail.com>
2026-04-27 19:18:47 +0700

    feat: add database for auth-service

---
+++ database/db_auth.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE IF NOT EXISTS db_auth;
+USE db_auth;
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('Mahasiswa','Admin','Unit') DEFAULT 'Mahasiswa',
+  `github_id` varchar(100) DEFAULT NULL,
+  `profile_pic` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `password` (`password`),
+  ADD UNIQUE KEY `github_id` (`github_id`);
+
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
